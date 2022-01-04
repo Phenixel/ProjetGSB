e3d7ea5cb9e2aa1b70f223a88b3edfaa7261fdb0
PhenYonathan <yonathancardoso@outlook.fr>
2022-01-04 11:39:09 +0100

    ajout prescrire

--- src/Entity/Prescrire.java
+++ src/Entity/Prescrire.java
@@ -10,5 +10,53 @@ package Entity;
  */
 public class Prescrire {
     
+    private int med_depotlegal;
+    private int tin_code;
+    private int dos_code;
+    private String pre_posologie;
+    
+    public Prescrire(int unMedDepotLegal, int unTinCode, int unDosCode, String unePrePosologie){
+        med_depotlegal = unMedDepotLegal;
+        tin_code = unTinCode;
+        dos_code = unDosCode;
+        pre_posologie = unePrePosologie;
+    }
+
+    /**
+     * @return the med_depotlegal
+     */
+    public int getMed_depotlegal() {
+        return med_depotlegal;
+    }
+
+    /**
+     * @return the tin_code
+     */
+    public int getTin_code() {
+        return tin_code;
+    }
+
+    /**
+     * @return the dos_code
+     */
+    public int getDos_code() {
+        return dos_code;
+    }
+
+    /**
+     * @return the pre_posologie
+     */
+    public String getPre_posologie() {
+        return pre_posologie;
+    }
+
+    /**
+     * @param pre_posologie the pre_posologie to set
+     */
+    public void setPre_posologie(String pre_posologie) {
+        this.pre_posologie = pre_posologie;
+    }
+    
+    
     
 }
