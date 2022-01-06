0e88251cb3b98504088c430f4bb0e25ad27f0eac
PhenYonathan <yonathancardoso@outlook.fr>
2022-01-06 10:26:32 +0100

    centrer ajout medic

--- src/Vue/frmAjoutMedic.java
+++ src/Vue/frmAjoutMedic.java
@@ -201,6 +201,7 @@ public class frmAjoutMedic extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnAddMedicMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAddMedicMouseClicked
